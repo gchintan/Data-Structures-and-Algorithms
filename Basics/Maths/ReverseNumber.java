1abbc853daf060560306d3c0d7c511bc70c1c5da
Chintan G <chintanchintu679@gmail.com>
2025-04-16 06:58:28 +0530

    Basic math

---
+++ Basics/Maths/ReverseNumber.java
@@ -0,0 +1,20 @@
+package Basics.Maths;
+
+public class ReverseNumber {
+
+    static int reverseNum(int n){
+        int rev=0;
+        while(n>0){
+            int digit = n%10;
+            n=n/10;
+            rev=rev*10+digit;
+        }
+        return rev;
+    }
+
+    public static void main(String[] args){
+        int n=12345;
+        System.out.print("Reverse of number "+ n + " is :"+reverseNum(n));
+    }
+
+}
