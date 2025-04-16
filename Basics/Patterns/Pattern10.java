405216c4036ea815c09f3957d8091402b027c304
Chintan G <chintanchintu679@gmail.com>
2025-04-16 06:26:09 +0530

    Basic patterns

---
+++ Basics/Patterns/Pattern10.java
@@ -0,0 +1,40 @@
+package Basics.Patterns;
+
+public class Pattern10 {
+    static void uprightPyramid(int n){
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n-i-1;j++){
+                System.out.print(" ");
+            }
+            for(int j=0;j<2*i+1;j++){
+                System.out.print("*");
+            }
+            for(int j=0;j<n-i-1;j++){
+                System.out.print(" ");
+            }
+            System.out.println();
+        }
+    }
+
+    static void invertedPyramid(int n){
+        for(int i=0;i<n;i++){
+            for(int j=0;j<i;j++){
+                System.out.print(" ");
+            }
+            for(int j=0;j<2*(n-i)-1;j++){
+                System.out.print("*");
+            }
+            for(int j=0;j<i;j++){
+                System.out.print(" ");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void main(String[] args){
+        int n=5;
+        uprightPyramid(5);
+        invertedPyramid(5);
+
+    }
+}
