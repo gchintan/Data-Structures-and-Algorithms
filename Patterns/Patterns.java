62c44e45684c37bc468374df14e3faec28d8459f
Chintan G <chintanchintu679@gmail.com>
2025-04-16 05:40:41 +0530

    patterns

--- Patterns/Patterns.java
+++ Patterns/Patterns.java
@@ -108,6 +108,62 @@ public class Patterns {
         }
     }
 
+    static void pattern7(int n){
+        /*
+            *
+           ***
+          *****
+         *******
+
+         */
+
+        for(int i=0;i<n;i++){
+            for (int j=0;j<n-i-1;j++){
+                System.out.print(" ");
+            }
+            for(int j=0;j<2*i+1;j++){
+                System.out.print("*");
+            }
+            for(int j=0;j<n-i-1;j++){
+                System.out.print(" ");
+            }
+            System.out.println();
+        }
+    }
+
+    static void pattern8(int n){
+        /*
+        *******
+         *****
+          ***
+           *
+
+         */
+        for(int i=0;i<n;i++){
+            for(int j=0;j<i;j++){
+                System.out.print(" ");
+            }
+            for(int j=0;j<2*(n-i)-1;j++){
+                System.out.print("*");
+            }
+            for(int j=0;j<i;j++){
+                System.out.print(" ");
+            }
+            System.out.println();
+        }
+    }
+
+    static void pattern9(int n){
+        for(int i=1;i<=2*n-1;i++){
+            int stars=i;
+            if(i>n){stars=2*n-i;}
+            for(int j=1;j<=stars;j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+
 
     public static void main(String[] args){
         int n=5;
@@ -117,6 +173,9 @@ public class Patterns {
         pattern4(n);
         pattern5(n);
         pattern6(n);
+        pattern7(n);
+        pattern8(n);
+        pattern9(n);
 
     }
 
